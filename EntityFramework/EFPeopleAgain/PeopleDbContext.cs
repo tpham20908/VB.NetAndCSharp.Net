6ebfe958a5426ce6d6e1453622b9dfab60c5b404
unknown <tpham20908@gmail.com>
2018-04-04 15:28:36 -0400

    EFPeopleAgain starting

---
+++ EntityFramework/EFPeopleAgain/PeopleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFPeopleAgain
+{
+    class PeopleDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
